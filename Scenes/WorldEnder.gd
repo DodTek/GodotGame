0a6b08ed56f11dd0d17cf91671345e56b0404fbd
Donnchadh O'Donoghue <donnodon11@gmail.com>
2019-05-20 23:14:07 +0100

    base(fixed)

--- Scenes/WorldEnder.gd
+++ Scenes/WorldEnder.gd
@@ -1,9 +1,10 @@
 #worldender.gd
 extends Area2D
 
+export(String,FILE, "*.tscn") var world_scene
 func _physics_process(delta):
 	var bodies = get_overlapping_bodies()
 	print(bodies)
 	for body in bodies:
 		if body.name == "Player":
-			get_tree().change_scene("Scenes/World2.tscn")
+			get_tree().change_scene(world_scene)
